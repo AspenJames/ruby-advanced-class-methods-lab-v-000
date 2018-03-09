69ea9ef757a85d6688dc6a9c99a37619ec2dcadf
Aspen James <aspenjames@tqca.org>
2018-03-09 04:30:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,41 @@
+# class Song
+#   attr_accessor :name, :artist_name
+#   @@all = []
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def save
+#     self.class.all << self
+#   end
+# 
+#   def self.create
+#     song = Song.new
+#     song.save
+#     song
+#   end
+# 
+#   def self.new_by_name(name)
+#     song = Song.create
+#     song.name = name
+#   end
+# 
+#   def self.find_by_name(name)
+#     @@all.detect{|s| s.name == name}
+#   end
+# 
+#   def self.find_or_create_by_name(name)
+#     self.class.find_by_name || self.class.create_by_name
+#   end
+# 
+#   def self.alphabetical
+#     songs = self.class.all.select{|s| s.name}.sort
+#   end
+# end
+
+
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -16,42 +54,4 @@ class Song
     song
   end
 
-  def self.new_by_name(name)
-    song = Song.create
-    song.name = name
-  end
-
-  def self.find_by_name(name)
-    @@all.detect{|s| s.name == name}
-  end
-
-  def self.find_or_create_by_name(name)
-    self.class.find_by_name || self.class.create_by_name
-  end
-
-  def self.alphabetical
-    songs = self.class.all.select{|s| s.name}.sort
-  end
 end
-
-
-
-# class Song
-#   attr_accessor :name, :artist_name
-#   @@all = []
-#
-#   def self.all
-#     @@all
-#   end
-#
-#   def save
-#     self.class.all << self
-#   end
-#
-#   def self.create
-#     song = Song.new
-#     song.save
-#     song
-#   end
-#
-# end
