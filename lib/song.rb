cbdc149f53ae772966e2beff1a463b1ee6282dd8
Aspen James <aspenjames@tqca.org>
2018-03-09 04:20:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,35 +1,51 @@
+# class Song
+#   attr_accessor :name, :artist_name
+#   @@all = []
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def save
+#     self.class.all << self
+#   end
+# 
+#   def self.create
+#     song = Song.new
+#     self.class.all << self
+#     song
+#   end
+# 
+#   def create_by_name(name)
+#     self.create
+#     self.name = name
+#   end
+# 
+#   def self.find_by_name(name)
+#     @@all.detect{|s| s.name == name}
+#   end
+# 
+#   def self.find_or_create_by_name(name)
+#     self.class.find_by_name || self.class.create_by_name
+#   end
+# 
+#   def self.alphabetical
+#     songs = self.class.all.select{|s| s.name}.sort
+#   end
+# end
+
+
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
-  def self.create
-    song = Song.new
-    self.class.all << self
-    song
-  end
-
-  def create_by_name(name)
-    self.create
-    self.name = name
-  end
-
-  def self.find_by_name(name)
-    @@all.detect{|s| s.name == name}
-  end
-
-  def self.find_or_create_by_name(name)
-    self.class.find_by_name || self.class.create_by_name
-  end
-
-  def self.alphabetical
-    songs = self.class.all.select{|s| s.name}.sort
-  end
+ 
 end
