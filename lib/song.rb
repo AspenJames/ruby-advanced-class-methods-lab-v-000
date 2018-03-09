22d0a6a6c13b959d3367d952307159c79e14ecf3
Aspen James <aspenjames@tqca.org>
2018-03-09 04:34:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,34 +1,34 @@
 # class Song
 #   attr_accessor :name, :artist_name
 #   @@all = []
-# 
+#
 #   def self.all
 #     @@all
 #   end
-# 
+#
 #   def save
 #     self.class.all << self
 #   end
-# 
+#
 #   def self.create
 #     song = Song.new
 #     song.save
 #     song
 #   end
-# 
+#
 #   def self.new_by_name(name)
 #     song = Song.create
 #     song.name = name
 #   end
-# 
+#
 #   def self.find_by_name(name)
 #     @@all.detect{|s| s.name == name}
 #   end
-# 
+#
 #   def self.find_or_create_by_name(name)
 #     self.class.find_by_name || self.class.create_by_name
 #   end
-# 
+#
 #   def self.alphabetical
 #     songs = self.class.all.select{|s| s.name}.sort
 #   end
@@ -52,6 +52,11 @@ class Song
     song = Song.new
     song.save
     song
+  end
+
+  def self.new_by_name(name)
+    song = Song.create
+    @name = name
   end
 
 end
